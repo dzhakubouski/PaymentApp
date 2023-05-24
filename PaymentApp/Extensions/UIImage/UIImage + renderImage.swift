85d4ab94855cc561af5954faa4abc8352b0d2540
Uladzislau Yakubouski <uladzislau.yakubouski@lemellabs.com>
2023-05-24 20:44:25 +0300

    adding render image to ui image extension

---
+++ PaymentApp/Extensions/UIImage/UIImage + renderImage.swift
@@ -0,0 +1,15 @@
+//
+//  UIImage + renderImage.swift
+//  PaymentApp
+//
+//  Created by Admin on 24.05.23.
+//
+
+import UIKit
+
+extension UIImage {
+    
+    static func renderImage(forName name: String) -> UIImage? {
+        return UIImage(named: name)
+    }
+}
