42c29ee251351bd79f34d79239930c9dbe04bb9f
Uladzislau Yakubouski <uladzislau.yakubouski@lemellabs.com>
2023-05-24 20:48:54 +0300

    adding constants

---
+++ PaymentApp/Constants.swift
@@ -0,0 +1,34 @@
+//
+//  Constants.swift
+//  PaymentApp
+//
+//  Created by Admin on 23.05.23.
+//
+
+import Foundation
+
+enum Constants {
+    
+    enum ViewControllerTitles {
+        static let paymentViewController: String = "Account"
+    }
+    
+    enum ButtonTitles {
+        static let paymentButtonTitle: String = "Pay"
+    }
+    
+    enum ImageNames {
+        static let paymentCardImage: String = "paymentCardImage"
+        static let flightTicketImage: String = "flightTicket"
+        static let trainTicketImage: String = "trainTicket"
+        static let busTicketImage: String = "busTicket"
+        static let cinemaTicket: String = "cinemaTicket"
+        static let restaurantBooking: String = "restaurantBooking"
+        static let hotelBooking: String = "hotelBooking"
+    }
+    
+    enum StaticLabelTitles {
+        static let proccessingPayment: String = "Processing payment"
+        static let paymentCompleted: String = "Payment completed!"
+    }
+}
